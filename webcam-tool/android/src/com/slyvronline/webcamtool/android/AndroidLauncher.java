079d8ddcade82325e654a4319b4d97e993184458
Matthew Schrum <mschrum89@gmail.com>
2015-10-30 09:35:07 -0500

    Initial commit

---
+++ webcam-tool/android/src/com/slyvronline/webcamtool/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.slyvronline.webcamtool.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.slyvronline.webcamtool.Main;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Main(), config);
+	}
+}
